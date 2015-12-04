c04ad5104abff289b73decfe38e89be18b14d267
David Siekut <davidsiekut@gmail.com>
2015-12-04 00:36:00 -0500

    Don't ask, though; they'll never tell

--- src/day2/day2.js
+++ src/day2/day2.js
@@ -1,20 +1,39 @@
 'use strict'
 
 var rl = require('readline').createInterface({
-  input: require('fs').createReadStream('./src/day2/day2.txt')
+  input: require('fs').createReadStream('./src/day2/input.txt')
 })
 
 let wrapping = 0
+let ribbon = 0
 
 rl.on('line', function (line) {
-  let s = line.split('x')
-  let l = +s[0]
-  let w = +s[1]
-  let h = +s[2]
+  let split = line.split('x')
+  let l = +split[0]
+  let w = +split[1]
+  let h = +split[2]
+
   wrapping += 2 * l * w + 2 * w * h + 2 * h * l
   wrapping += Math.min(l * w, w * h, h * l)
+
+  let s = smallest(l, w, h)
+  let a = s[0]
+  let b = s[1]
+
+  ribbon += a + a + b + b
+  ribbon += l * w * h
 })
 
 rl.on('close', function () {
   console.log(wrapping)
+  console.log(ribbon)
+
+  rl.close()
 })
+
+function smallest (l, w, h) {
+  if (h < Math.max(l, w)) {
+    return [Math.min(l, w), h]
+  }
+  return [l, w]
+}
