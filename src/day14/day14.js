eb43e8f0a83ebc10675aadbca0dd42a09193b466
David Siekut <davidsiekut@gmail.com>
2015-12-15 01:14:54 -0500

    Reindeer games

---
+++ src/day14/day14.js
@@ -0,0 +1,28 @@
+'use strict'
+
+let Reindeer = require('./reindeer.js')
+
+let sleigh = []
+sleigh.push(new Reindeer('vixen', 8, 8, 53))
+sleigh.push(new Reindeer('blitzen', 13, 4, 49))
+sleigh.push(new Reindeer('rudolph', 20, 7, 132))
+sleigh.push(new Reindeer('cupid', 12, 4, 43))
+sleigh.push(new Reindeer('donner', 9, 5, 38))
+sleigh.push(new Reindeer('dasher', 10, 4, 37))
+sleigh.push(new Reindeer('comet', 3, 37, 76))
+sleigh.push(new Reindeer('prancer', 9, 12, 97))
+sleigh.push(new Reindeer('dancer', 37, 1, 36))
+
+let t = 0
+let tmax = 2503
+
+while (t <= tmax) {
+  for (let i = 0; i < sleigh.length; i++) {
+    sleigh[i].update()
+  }
+  t++
+}
+
+for (let i = 0; i < sleigh.length; i++) {
+  console.log(sleigh[i].position)
+}
---
+++ src/day14/reindeer.js
@@ -0,0 +1,40 @@
+var reindeer = Reindeer.prototype
+
+function Reindeer (n, sp, st, r) {
+  this.name = n
+  this.speed = sp
+  this.stamina = st
+  this.recovery = r
+
+  this.position = 0
+  this.cstamina = st
+  this.crecovery = 0
+  this.resting = false
+}
+
+reindeer.update = function () {
+  if (!this.resting) {
+    this.go()
+  } else {
+    this.recover()
+  }
+}
+
+reindeer.go = function () {
+  this.position += this.speed
+  this.cstamina--
+  if (this.cstamina === 0) {
+    this.resting = true
+  }
+}
+
+reindeer.recover = function () {
+  this.crecovery++
+  if (this.crecovery === this.recovery) {
+    this.cstamina = this.stamina
+    this.crecovery = 0
+    this.resting = false
+  }
+}
+
+module.exports = Reindeer
